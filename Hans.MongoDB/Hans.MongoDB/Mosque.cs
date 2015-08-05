673fa3628d5dd56d342acabfbb8090579daa6f0a
Muhamad Farihan <farihan@gmail.com>
2015-08-05 23:27:02 +0800

    add query for mosque

---
+++ Hans.MongoDB/Hans.MongoDB/Mosque.cs
@@ -0,0 +1,38 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hans.MongoDB
+{
+    public class Mosque
+    {
+        [BsonId]
+        [BsonElement("_id")]
+        public ObjectId ID { get; set; }
+
+        [BsonElement("nama")]
+        public string Name { get; set; }
+
+        [BsonElement("alamat")]
+        public string Address { get; set; }
+
+        [BsonElement("poskod")]
+        public string Postcode { get; set; }
+
+        [BsonElement("negeri")]
+        public string State { get; set; }
+
+        [BsonElement("daerah")]
+        public string District { get; set; }
+
+        [BsonElement("telefon")]
+        public string Contact { get; set; }
+
+        [BsonElement("jenis")]
+        public string Type { get; set; }
+    }
+}
--- Hans.MongoDB/Hans.MongoDB/Program.cs
+++ Hans.MongoDB/Hans.MongoDB/Program.cs
@@ -10,13 +10,62 @@ namespace Hans.MongoDB
     {
         static void Main(string[] args)
         {
+            LoadMosques();
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+
+        private static void LoadMosques()
+        {
+            IList<Mosque> list1 = null;
+            IList<Mosque> list2 = null;
+
+            Task.Run(async () =>
+            {
+                list1 = await GetMosqueList();
+                list2 = await GetMosqueListBy();
+            }).Wait();
+
+            foreach (var item in list1)
+            {
+                Console.WriteLine("{0} - {1} - {2}", item.Name, item.District, item.State);
+            }
+
+            Console.WriteLine();
+
+            foreach (var item in list2)
+            {
+                Console.WriteLine("{0} - {1} - {2}", item.Name, item.District, item.State);
+            }
+        }
+
+        private static async Task<IList<Mosque>> GetMosqueList()
+        {
+            var repo = new Repository<Mosque>("mosques");
+            var list = await repo.FindAll();
+            return list.OrderBy(x => x.Name)
+                .ToList();
+        }
+
+        private static async Task<IList<Mosque>> GetMosqueListBy()
+        {
+            var repo = new Repository<Mosque>("mosques");
+            var list = await repo.FindAllBy(x => x.State.ToLower() == "perak");
+            return list.OrderBy(x => x.Name)
+                .ToList();
+        }
+
+        private static void LoadProducts()
+        {
             IList<Product> list1 = null;
             IList<Product> list2 = null;
 
             Task.Run(async () =>
             {
-                list1 = await GetList();
-                list2 = await GetListBy();
+                list1 = await GetProductList();
+                list2 = await GetProductListBy();
             }).Wait();
 
             foreach (var item in list1)
@@ -29,13 +78,9 @@ namespace Hans.MongoDB
             {
                 Console.WriteLine("{0} - {1} - {2}", item.ProductID, item.ProductName, item.UnitsInStock);
             }
-
-            Console.WriteLine();
-            Console.WriteLine("Press any key to exit...");
-            Console.ReadKey();
         }
 
-        public static async Task<IList<Product>> GetList()
+        private static async Task<IList<Product>> GetProductList()
         {
             var repo = new Repository<Product>("products");
             var list = await repo.FindAll();
@@ -43,7 +88,7 @@ namespace Hans.MongoDB
                 .ToList();
         }
         
-        public static async Task<IList<Product>> GetListBy()
+        private static async Task<IList<Product>> GetProductListBy()
         {
             var repo = new Repository<Product>("products");
             var list = await repo.FindAllBy(x => x.UnitsInStock == 0);
